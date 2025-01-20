940919e1e5c0d7859ef4b738d51469c963d01caf
shiva <shivatadigadapa@gmail.com>
2025-01-20 13:26:11 +0530

    Add SmoothScrolling component for enhanced smooth scrolling experience

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import SmoothScrolling from './utils/Smooth.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <SmoothScrolling>
+      <App />
+    </SmoothScrolling>
   </StrictMode>,
 )
